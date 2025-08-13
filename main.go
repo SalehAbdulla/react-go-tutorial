13870f1b185176cc4040e48f498666f96ff15650
Saleh Abdulla <salehabdullaict@gmail.com>
2025-08-13 14:36:19 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main(){
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg":"Hello World"})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
